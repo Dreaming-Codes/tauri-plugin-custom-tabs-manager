f79efb002c8698d5d99bb4e8bff323c826001008
DreamingCodes <dev@dreaming.codes>
2023-07-01 07:22:02 +0200

    "Enhance CustomTabsManager with custom tabs lifecycle management"
    This commit enhances our CustomTabsManager by adding a crucial part of the lifecycle that is the connection/disconnection to the CustomTabsService. The change includes warming up the Browser (to speed up the launching of the URL) and creating new sessions for each URL launching. This was done to improve the performance and user experience when launching URLs in our app. We've also replaced the simple CustomTabsIntent launching with a more flexible approach allowing customization in the future.

--- android/src/main/java/CustomTabsManager.kt
+++ android/src/main/java/CustomTabsManager.kt
@@ -1,13 +1,38 @@
 package codes.dreaming.plugin.CustomTabsManager
 
 import android.app.Activity
+import android.content.ComponentName
 import android.net.Uri
-import android.util.Log
+import androidx.browser.customtabs.CustomTabsCallback
+import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.browser.customtabs.CustomTabsSession
 
 class CustomTabsManager(private val activity: Activity) {
+    private var customTabsClient: CustomTabsClient? = null
+    private var customTabsSession: CustomTabsSession? = null
+    private var connection: CustomTabsServiceConnection = object : CustomTabsServiceConnection() {
+        override fun onCustomTabsServiceConnected(name: ComponentName, client: CustomTabsClient) {
+            customTabsClient = client
+            customTabsClient?.let {
+                it.warmup(0L)
+                customTabsSession = it.newSession(null)
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            customTabsClient = null
+        }
+    }
+
+    init {
+        CustomTabsClient.bindCustomTabsService(activity, "com.android.chrome", connection)
+    }
+
     fun openCustomTabSimple(url: String) {
-        val intent = CustomTabsIntent.Builder().build()
-        intent.launchUrl(activity, Uri.parse(url))
+        val builder = CustomTabsIntent.Builder(customTabsSession)
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(activity, Uri.parse(url))
     }
 }
